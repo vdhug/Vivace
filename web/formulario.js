b9397bbd971defc4ae612e2e2c03a4a30db2f41c
vdhug <vitorhug97@gmail.com>
2018-10-06 03:20:45 -0300

    Adicionando funcionalidade de enviar email

--- web/formulario.js
+++ web/formulario.js
@@ -1,44 +1,7 @@
-$(".rslides").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
-});
-
-$(function() {
-  $(".rslides").responsiveSlides();
-});
-
-Visibility.onVisible(function(){
-	//Animações Home
-	setTimeout(function () {
-		$(".introducao h1").addClass("animated fadeInDown");
-	}, 400);
-	setTimeout(function () {
-		$(".introducao blockquote").addClass("animated fadeInDown");
-	}, 800);
-	setTimeout(function () {
-		$(".introducao .btn").addClass("animated fadeInDown");
-	}, 1200);
-	setTimeout(function () {
-		$(".animar").addClass("animated fadeInDown");
-	}, 1600);
-
-	//Animações internas
-	setTimeout(function () {
-		$(".introducao-interna h1").addClass("animated fadeInDown");
-	}, 400);
-	setTimeout(function () {
-		$(".introducao-interna p").addClass("animated fadeInDown");
-	}, 800);
-	setTimeout(function () {
-		$(".animar-interno").addClass("animated fadeInDown");
-	}, 1200);
-});
-
 // Formulario
 
 $('.formphp').on('submit', function() {
-	var emailContato = "vitorhug97@gmail.com"; // Escreva aqui o seu e-mail
+	var emailContato = ""; // Escreva aqui o seu e-mail
 
 	var that = $(this),
 			url = that.attr('action'),
