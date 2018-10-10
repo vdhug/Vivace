a5aece5fb7fc12d61d4601d9fdcf7e0168d5ec54
vdhug <vitorhug97@gmail.com>
2018-10-09 22:43:05 -0300

    Conserto de overflow de imagens, quebra de UI na secao de reviews de clientes e ajuste de transicao do slideshow das reviews

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 $(".rslides").responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
+  speed: 2000,            // Integer: Speed of the transition, in milliseconds
+  timeout: 30000,          // Integer: Time between slide transitions, in milliseconds
 });
 
 $(function() {
