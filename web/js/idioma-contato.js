d6cf760a225ca624b36a56da022561eaebf53b59
vdhug <vitorhug97@gmail.com>
2018-10-25 22:25:14 -0300

    Finalizando traducao de todo site

---
+++ web/js/idioma-contato.js
@@ -0,0 +1,151 @@
+var idioma = "portugues";
+
+document.addEventListener('DOMContentLoaded', function() {
+    if ((getQueryStringValue("lg")) != ""){
+    	idioma = getQueryStringValue("lg");
+    }
+    if(idioma === "espanhol"){
+    	espanhol();
+    }
+    else if (idioma === "ingles"){
+    	ingles();
+    }
+
+});
+
+
+function getQueryStringValue (key) {  
+  return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
+} 
+
+// Function to change the currently language to PT-BR
+function portugues() {
+    idioma = "portugues";
+    traduzPortugues();
+	return false;
+}
+
+// Function to change the currently language to ES_MX
+function espanhol() {
+    idioma = "espanhol";
+    traduzEspanhol();
+	return false;
+}
+
+// Function to change the currently language EN-US
+function ingles() {
+    idioma = "ingles";
+    traduzIngles();
+	return false;
+}
+
+function traduzEspanhol() {
+	//Traduz todo menu
+	document.querySelector('#menu-inicio').innerHTML = "Inicio";
+	document.querySelector('#menu-sobre').innerHTML = "Sobre";
+	document.querySelector('#menu-servicos').innerHTML = "Servicios";
+	document.querySelector('#menu-clientes').innerHTML = "Clientes";
+	document.querySelector('#menu-contato').innerHTML = "Contacto";
+
+	//Traduz toda introducao
+	document.querySelector('#introducao-titulo').innerHTML = "Contato";
+	document.querySelector('#introducao-descricao').innerHTML = "Tira tus dudas con nosotros";
+
+
+	//Traduz form orcamento
+	document.querySelector('#orcamento-nome').innerHTML = "Nombre";
+	document.querySelector('#orcamento-email').innerHTML = "Email";
+	document.querySelector('#orcamento-telefone').innerHTML = "Teléfono";
+	document.querySelector('#orcamento-mensagem').innerHTML = "Mensaje";
+	document.querySelector('#enviar').innerHTML = "Enviar";
+	document.querySelector('#dados-titulo').innerHTML = "Datos";
+	document.querySelector('#instrucoes-titulo').innerHTML = "Pieza su presupuesto";
+	document.querySelector('#instrucoes-descricao').innerHTML = "Información sugerida para incluir en el mensaje";
+	document.querySelector('#instrucao1').innerHTML = "- Tamaño de la construcción";
+	document.querySelector('#instrucao2').innerHTML = "- Tipo (residencial o comercial)";
+	document.querySelector('#instrucao3').innerHTML = "- Servicio deseado";
+
+
+
+	//Traduz footer
+	document.querySelector('#footer-titulo').innerHTML = "Nuestra Historia";
+	document.querySelector('#footer-historia').innerHTML = "La Vivace tiene una sólida capacidad técnica y alta competencia en gestionar proyectos, con el foco en las necesidades de los clientes y con soluciones de alto valor agregado.";
+	document.querySelector('#footer-contato').innerHTML = "Contacto";
+	document.querySelector('#footer-redes-sociais').innerHTML = "Síguenos";
+	document.querySelector('#footer-direitos').innerHTML = "Vivace Engenharia LTDA - Algunos derechos reservados.";
+
+}
+
+
+function traduzIngles() {
+	//Traduz todo menu
+	document.querySelector('#menu-inicio').innerHTML = "Index";
+	document.querySelector('#menu-sobre').innerHTML = "About";
+	document.querySelector('#menu-servicos').innerHTML = "Services";
+	document.querySelector('#menu-clientes').innerHTML = "Clients";
+	document.querySelector('#menu-contato').innerHTML = "Contact";
+
+	//Traduz toda introducao
+	document.querySelector('#introducao-titulo').innerHTML = "Contact";
+	document.querySelector('#introducao-descricao').innerHTML = "Enter in contact with us";
+
+	
+	//Traduz form orcamento
+	document.querySelector('#orcamento-nome').innerHTML = "Name";
+	document.querySelector('#orcamento-email').innerHTML = "Email";
+	document.querySelector('#orcamento-telefone').innerHTML = "Phone";
+	document.querySelector('#orcamento-mensagem').innerHTML = "Message";
+	document.querySelector('#enviar').innerHTML = "Send";
+	document.querySelector('#dados-titulo').innerHTML = "Data";
+	document.querySelector('#instrucoes-titulo').innerHTML = "Ask your budget";
+	document.querySelector('#instrucoes-descricao').innerHTML = "Suggested information to include in the message";
+	document.querySelector('#instrucao1').innerHTML = "- Size of the building";
+	document.querySelector('#instrucao2').innerHTML = "- Type (residential or commercial)";
+	document.querySelector('#instrucao3').innerHTML = "- Desired Service";
+
+	
+	//Traduz footer
+	document.querySelector('#footer-titulo').innerHTML = "Our Story";
+	document.querySelector('#footer-historia').innerHTML = "Vivace has proven technical capacity and high competence in managing projects, focusing on customer needs and solutions with high added value.";
+	document.querySelector('#footer-contato').innerHTML = "Contact";
+	document.querySelector('#footer-redes-sociais').innerHTML = "Social Media";
+	document.querySelector('#footer-direitos').innerHTML = "Vivace Engenharia LTDA - Some rights reserved.";
+
+}
+
+
+function traduzPortugues() {
+	//Traduz todo menu
+	document.querySelector('#menu-inicio').innerHTML = "Início";
+	document.querySelector('#menu-sobre').innerHTML = "Sobre";
+	document.querySelector('#menu-servicos').innerHTML = "Serviços";
+	document.querySelector('#menu-clientes').innerHTML = "Clientes";
+	document.querySelector('#menu-contato').innerHTML = "Contato";
+
+	//Traduz toda introducao
+	document.querySelector('#introducao-titulo').innerHTML = "Contato";
+	document.querySelector('#introducao-descricao').innerHTML = "Tire suas dúvidas com a gente";
+
+	
+	//Traduz form orcamento
+	document.querySelector('#orcamento-nome').innerHTML = "Nome";
+	document.querySelector('#orcamento-email').innerHTML = "Email";
+	document.querySelector('#orcamento-telefone').innerHTML = "Telefone";
+	document.querySelector('#orcamento-mensagem').innerHTML = "Mensagem";
+	document.querySelector('#enviar').innerHTML = "Enviar";
+	document.querySelector('#dados-titulo').innerHTML = "Dados";
+	document.querySelector('#instrucoes-titulo').innerHTML = "Peça seu orçamento";
+	document.querySelector('#instrucoes-descricao').innerHTML = "Informações sugeridas para incluir na mensagem";
+	document.querySelector('#instrucao1').innerHTML = "- Tamanho da construção";
+	document.querySelector('#instrucao2').innerHTML = "- Tipo (residencial ou comercial)";
+	document.querySelector('#instrucao3').innerHTML = "- Serviço desejado";
+
+	
+	//Traduz footer
+	document.querySelector('#footer-titulo').innerHTML = "Nossa História";
+	document.querySelector('#footer-historia').innerHTML = "A Vivace possui comprovada capacidade técnica e alta competência em gerenciar projetos, com o foco nas necessidades dos clientes e com soluções de alto valor agregado.";
+	document.querySelector('#footer-contato').innerHTML = "Contato";
+	document.querySelector('#footer-redes-sociais').innerHTML = "Redes Sociais";
+	document.querySelector('#footer-direitos').innerHTML = "Vivace Engenharia LTDA - Alguns direitos reservados.";
+
+}
